a0a822edbd5865f594ea02baba0d4fdfab903220
João Vitor Dias <104777038+JoaoDias12@users.noreply.github.com>
2026-04-15 13:58:50 -0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -26,7 +26,7 @@ var persons = {
     MonthDayOff: "1",
     DayOffs: [],
     Double: false,
-    Times: ["F,3,1,1,1,3,3,F,3,1,3,1,1,1,F,F,1,3,3,1,1,3,F,3,2,1,2,3,3,F"],
+    Times: [""],
     Vacation: false
   },
   Eduardo: {
@@ -35,7 +35,7 @@ var persons = {
     MonthDayOff: "1",
     DayOffs: [],
     Double: false,
-    Times: ["2,1,3,1,F,3,2,1,1,3,1,F,F,3,3,1,1,1,1,F,1,1,3,2,1,1,F,F,2,1"],
+    Times: [""],
     Vacation: false
   },
   Pedro: {
@@ -44,7 +44,7 @@ var persons = {
     MonthDayOff: "1",
     DayOffs: [],
     Double: true,
-    Times: ["1,1,1,3,2,F,F,3,1,1,1,3,3,F,3,1,3,1,1,1,F,F,1,1,3,1,3,2,F,3"],
+    Times: [""],
     Vacation: false
   },
   Flavio: {
@@ -53,7 +53,7 @@ var persons = {
     MonthDayOff: "1",
     DayOffs: [],
     Double: false,
-    Times: ["F,2,3,3,3,1,1,F,3,2,3,1,3,1,F,F,3,3,3,2,2,3,F,3,3,3,3,1,3,F"],
+    Times: [""],
     Vacation: false
   },
   Ramon: {
@@ -62,7 +62,7 @@ var persons = {
     MonthDayOff: "1",
     DayOffs: [],
     Double: false,
-    Times: ["1,F,1,1,1,1,3,1,F,F,1,3,2,2,1,1,F,1,1,3,1,2,1,F,F,3,1,1,1,1"],
+    Times: [""],
     Vacation: false
   },
   Marya: {
@@ -71,7 +71,7 @@ var persons = {
     MonthDayOff: "1",
     DayOffs: [],
     Double: true,
-    Times: ["1,1,F,F,1,2,1,3,1,1,F,1,1,3,1,3,2,F,F,1,3,1,3,1,1,F,1,3,1,3"],
+    Times: [""],
     Vacation: false
   },
   Giovanna: {
@@ -80,7 +80,7 @@ var persons = {
     MonthDayOff: "1",
     DayOffs: [],
     Double: true,
-    Times: ["2,3,F,F,3,1,1,1,2,3,F,2,1,1,1,3,1,F,F,3,3,1,1,1,1,F,1,1,1,1"],
+    Times: [""],
     Vacation: false
   }
 };
